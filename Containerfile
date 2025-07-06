c5eb5195aabbff3055c7bebaadf6206c7622cdd9
Sergey Popov <olleman@gmail.com>
2025-07-06 13:29:15 +0200

    strange

--- Containerfile
+++ Containerfile
@@ -2,6 +2,7 @@
 FROM scratch AS ctx
 COPY build_files /
 
+
 # Base Image
 FROM ghcr.io/ublue-os/ucore-minimal:stable
 
@@ -18,6 +19,7 @@ FROM ghcr.io/ublue-os/ucore-minimal:stable
 ## make modifications desired in your image and install packages by modifying the build.sh script
 ## the following RUN directive does all the things required to run "build.sh" as recommended.
 
+#COPY lol.txt /usr/local/bin/lol
 RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=cache,dst=/var/cache \
     --mount=type=cache,dst=/var/log \
--- build_files/build.sh
+++ build_files/build.sh
@@ -18,12 +18,11 @@ echo "well here I am being a silly boy YEEHAW" >/etc/trash2/message.txt
 mkdir -p /etc/systemd/system/rpm-ostreed-automatic.timer.d/
 printf '[Timer]\nOnUnitInactiveSec=1min\nPersistent=true\n' >/etc/systemd/system/rpm-ostreed-automatic.timer.d/override.conf
 
-# install k3s I guess?
-
-export INSTALL_K3S_CHANNEL=latest
-#export INSTALL_K3S_EXEC="server --node-ip=\"$NODE_IP\" --advertise-address=\"$NODE_IP\""
-export K3S_KUBECONFIG_MODE=644
-./ctx/k3s.sh
+mkdir -p /var/usr/local/bin
+echo "you've reached me" >/var/usr/local/bin/lol
+cat >/usr/lib/wtf.conf <<EOF
+  echo "lol"
+EOF
 #curl -sfL https://get.k3s.io | sh -
 # Use a COPR Example:
 #
